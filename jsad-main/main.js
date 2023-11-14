b2cfebde37efe8198493234d75e0fecc09fadfc5
miakhalifa24 <106820283+miakhalifa24@users.noreply.github.com>
2023-11-14 09:05:46 -0300

    Add files via upload

---
+++ jsad-main/main.js
@@ -0,0 +1,22 @@
+function playAudio (audioID){
+    alert (audio);
+
+const audio = document.getElementById(audioID)
+
+  if(audio){
+     audio.play();
+  }else{
+      alert(audioID)
+  }
+
+
+
+
+
+
+
+
+
+
+
+}
